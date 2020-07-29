b62af1d54a59c6d7f3310f32f1b081b1a2bf2b7c
Jesse Galloway <jsg220@lehigh.edu>
2020-07-28 21:45:23 -0400

    Revamped incident logging system:

--- track/postprocessing.py
+++ track/postprocessing.py
@@ -7,12 +7,14 @@ import cv2
 import random
 import csv
 import string
+import datetime
 
 class Job:
     SESSION_NAME = 'script'
-    START_TIME = 20000 #20 seconds, the time we initially jump to
     BRACKET_BUFFER = 10
     WINDOW_NAME = 'window'
+    VIDEO_OUTPUT_FPS = 10
+    SECONDS_PER_FRAME = 4
 
     def __init__(self, data):
         self.path = data
@@ -58,64 +60,66 @@ class Job:
             xMax = ROI[0] + ROI[2] + buffer
             yMax = ROI[1] + ROI[3] + buffer
             return ((xMin < point[0] < xMax) and (yMin < point[1] < yMax))
-        
-        def linear_interpolation(trajectories):
-            interp = 0
-            for id in range(0, 2):
-                for axis in range(0, 2):
-                    for frame in range(len(trajectories)):
-                        if(np.isnan(trajectories[frame][id][axis])):
-                            lastGood = frame-1
-                            newGood = lastGood
-                            endFrame = frame
-                            while(np.isnan(trajectories[frame][id][axis])):
-                                if(frame == len(trajectories)-1):
-                                    print("we reached the end of the traj with no new good value, using uniform interpolation. Accuracy will suffer")
-                                    endFrame = frame
-                                    break
-                                frame += 1
-                            newGood = frame
-                            endFrame = frame
+        def interpolate(trajectories):
+            def linear_interpolation(trajectories):
+                interp = 0
+                for id in range(0, 2):
+                    for axis in range(0, 2):
+                        for frame in range(len(trajectories)):
+                            if(np.isnan(trajectories[frame][id][axis])):
+                                lastGood = frame-1
+                                newGood = lastGood
+                                endFrame = frame
+                                while(np.isnan(trajectories[frame][id][axis])):
+                                    if(frame == len(trajectories)-1):
+                                        print("we reached the end of the traj with no new good value, using uniform interpolation. Accuracy will suffer")
+                                        endFrame = frame
+                                        break
+                                    frame += 1
+                                newGood = frame
+                                endFrame = frame
 
-                            if (newGood == lastGood):
-                                for cr in range (lastGood, endFrame):
-                                    trajectories[cr][id][axis] = trajectories[lastGood][id][axis]
-                                    interp += 1
-                            else:
-                                step = (trajectories[newGood][id][axis] - trajectories[lastGood][id][axis])/(endFrame - lastGood)
-                                for cr in range (lastGood, endFrame):
-                                    trajectories[cr][id][axis] = trajectories[cr-1][id][axis] + step
-                                    interp += 1
-            print("Linear Interpolation finished. Filled ({}) values".format(interp))
-            print("\t{}/{} values linearly interpolated, {}%".format(interp, len(trajectories)*4, (interp/(len(trajectories)*4))*100))
-            return trajectories
+                                if (newGood == lastGood):
+                                    for cr in range (lastGood, endFrame):
+                                        trajectories[cr][id][axis] = trajectories[lastGood][id][axis]
+                                        interp += 1
+                                else:
+                                    step = (trajectories[newGood][id][axis] - trajectories[lastGood][id][axis])/(endFrame - lastGood)
+                                    for cr in range (lastGood, endFrame):
+                                        trajectories[cr][id][axis] = trajectories[cr-1][id][axis] + step
+                                        interp += 1
+                print("Linear Interpolation finished. Filled ({}) values".format(interp))
+                print("\t{}/{} values linearly interpolated, {}%".format(interp, len(trajectories)*4, (interp/(len(trajectories)*4))*100))
+                return trajectories
 
-        def prox_interpolation(trajectories):
-            failed_frames = 0
-            def copyGoodValues(frame):
-                success = True
-                #We'll only check the x coord since idtracker ai won't record just one axis
-                if (np.isnan((frame)[0][0]) and (not np.isnan(frame[1][0]))):
-                    #id 0 is gone but 1 is there, copy 1's values to 0
-                    frame[0] = frame[1]
-                elif (np.isnan((frame)[1][0]) and (not np.isnan(frame[0][0]))):
-                    frame[1] = frame[0]
-                else:
-                    success = False
-                return (success, frame)
+            def prox_interpolation(trajectories):
+                failed_frames = 0
+                def copyGoodValues(frame):
+                    success = True
+                    #We'll only check the x coord since idtracker ai won't record just one axis
+                    if (np.isnan((frame)[0][0]) and (not np.isnan(frame[1][0]))):
+                        #id 0 is gone but 1 is there, copy 1's values to 0
+                        frame[0] = frame[1]
+                    elif (np.isnan((frame)[1][0]) and (not np.isnan(frame[0][0]))):
+                        frame[1] = frame[0]
+                    else:
+                        success = False
+                    return (success, frame)
 
-            interp = 0
-            for frame in trajectories:
-                if np.isnan(np.sum(frame)):
-                    interp += 1
-                    success, frame = copyGoodValues(frame)
-                    if not success: failed_frames += 1
-            print("Proximity Interpolation finished. Filled ({}) values. ({}) Failed frames.".format(interp*2, failed_frames))
-            print("\t{}/{} values proximity interpolated, {}%".format(interp*2, len(trajectories)*4, (interp/(len(trajectories)*2))*100))
-            return trajectories
+                interp = 0
+                for frame in trajectories:
+                    if np.isnan(np.sum(frame)):
+                        interp += 1
+                        success, frame = copyGoodValues(frame)
+                        if not success: failed_frames += 1
+                print("Proximity Interpolation finished. Filled ({}) values. ({}) Failed frames.".format(interp*2, failed_frames))
+                print("\t{}/{} values proximity interpolated, {}%".format(interp*2, len(trajectories)*4, (interp/(len(trajectories)*2))*100))
+                return trajectories
 
+            return linear_interpolation(prox_interpolation(trajectories))
+            # trajectories = linear_interpolation(trajectories)
 
-        def black_white_id(trajectories):
+        def verify_and_swap_black_white(trajectories):
             black_to_id0 = dist(self.black, trajectories[0][0])
             print("Black is {} away from id0.".format(black_to_id0))
             black_to_id1 = dist(self.black, trajectories[0][1])
@@ -138,12 +142,48 @@ class Job:
             count = 0
             in_prox_last = False
             PROXIMITY_FRAME_RANGE = 10
+            incident_list = []
+            class Incident:
+                def __init__(self,start_frame,white_avg_vel_in,black_avg_vel_in):
+                    self.start_frame = start_frame
+                    self.start_time_video = datetime.timedelta(seconds=start_frame/Job.VIDEO_OUTPUT_FPS)
+                    self.start_time_trial = datetime.timedelta(seconds=start_frame*Job.SECONDS_PER_FRAME)
+                    if(black_avg_vel_in > white_avg_vel_in):
+                        self.initiator = "Black"
+                        self.initiator_confidence = black_avg_vel_in - white_avg_vel_in
+                    else:
+                        self.initiator = "White"
+                        self.initiator_confidence = white_avg_vel_in - black_avg_vel_in
+                    self.end_time_video = None
+                    self.end_time_trial = None
+                    self.duration_video = None
+                    self.duration_trial = None
+                def endIncident(self, end_frame, white_avg_vel_out, black_avg_vel_out):
+                    self.end_frame = end_frame
+                    self.end_time_video = datetime.timedelta(seconds=self.end_frame/Job.VIDEO_OUTPUT_FPS)
+                    self.end_time_trial = datetime.timedelta(seconds=self.end_frame*Job.SECONDS_PER_FRAME)
+                    self.duration_video = datetime.timedelta(seconds=(self.end_frame - self.start_frame)/Job.VIDEO_OUTPUT_FPS)
+                    self.duration_trial = datetime.timedelta(seconds=(self.end_frame - self.start_frame)*Job.SECONDS_PER_FRAME)
+                def prettyPrint(self):
+                        print('\tVideo start time: {}'.format(str(self.start_time_video)))
+                        print('\tTrial start time: {}'.format(str(self.start_time_trial)))
+                        print('\tInitiator: {}'.format(self.initiator))
+                        print('\t\tConfidence: {}'.format(round(self.initiator_confidence,1)))
+                        if(self.end_frame):
+                            print('\tVideo end time: {}'.format(str(self.end_time_video)))
+                            print('\tTrial end time: {}'.format(str(self.end_time_trial)))
+                            print('\tDuration video: {}'.format(str(self.duration_video)))
+                            print('\tDuration trial: {}'.format(str(self.duration_trial)))
+                        else:
+                            print("\tERROR: interaction has no end.")
+
+            def calculateAvgVelInRange(self, i, trajectories):
+
             for i in range(PROXIMITY_FRAME_RANGE, len(proximity)-PROXIMITY_FRAME_RANGE):
                 distance = dist(getBlack(trajectories[i]), getWhite(trajectories[i]))
                 proximity[i] = distance
-                if ( distance < self.proximity_range):
+                if (distance < self.proximity_range):
                     proximity[i] = distance
-                    count += 1
                     if not in_prox_last:
                         avg_pos = [(getBlack(trajectories[i])[0]+getWhite(trajectories[i])[0])/2, (getBlack(trajectories[i])[1]+getWhite(trajectories[i])[1])/2 ]
                         #The beetles have just entered proximity, who initiated?
@@ -155,22 +195,24 @@ class Job:
                             white_avg_vel += dist(getWhite(incident[j-1]), avg_pos) - dist(getWhite(incident[j]), avg_pos)
                         black_avg_vel = black_avg_vel/(len(incident)-1)
                         white_avg_vel = white_avg_vel/(len(incident)-1)
-                        print("Contact initiated, beetles are: {} pixels apart".format(dist(getBlack(trajectories[i]), getWhite(trajectories[i]))))
-                        if(black_avg_vel > white_avg_vel):
-                            print("Frame: {} Black initiates contact".format(i+self.startFrame))
-                        else:
-                            print("White intiates contact")
+                        this_incident = Incident(i, white_avg_vel, black_avg_vel)
+                        incident_list.append(this_incident)
                         in_prox_last = True
                 else:
+                    if in_prox_last:
+                        #The interaction just ended
+
+                        incident_list[-1].endIncident(i, ???????????????)
+                        print('Contact Incident {}:'.format(len(incident_list)))
+                        incident_list[-1].prettyPrint()
                     in_prox_last = False
-            print("frames in prox: {}/{}".format(count, len(trajectories)))
             return proximity
         
         def make_video(trajectories, proximities, video_name):
             vidcap = cv2.VideoCapture(self.videoFile)
             success,image = vidcap.read()
             height, width = image.shape[:2]
-            out = cv2.VideoWriter(video_name,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (width,height))
+            out = cv2.VideoWriter(video_name,cv2.VideoWriter_fourcc('M','J','P','G'), Job.VIDEO_OUTPUT_FPS, (width,height))
             vidcap.set(cv2.CAP_PROP_POS_FRAMES,self.startFrame)
             # index = 0
             for index in range(len(trajectories)):
@@ -228,15 +270,14 @@ class Job:
         #Crop the trajectories array down to the analysed part
         trajectories = all_trajectories[self.startFrame:self.videoEndFrame-1]
         print('---------- INTERPOLATION ----------')
-        trajectories = prox_interpolation(trajectories)
-        trajectories = linear_interpolation(trajectories)
-        trajectories = black_white_id(trajectories)
+        trajectories = verify_and_swap_black_white(interpolate(trajectories))
+        # trajectories = verify_and_swap_black_white(trajectories)
         print('---------- PROXIMITY ANALYSIS ----------')
         proximities = proximity_detection(trajectories)
         save_csv(trajectories, proximities, "post_proc_output.csv")
         print('Data output saved to {}'.format("post_proc_output.csv"))
-        make_video(trajectories, proximities, "post_proc_output.avi")
-        print('Annotated video saved to {}'.format("post_proc_output.avi"))
+        # make_video(trajectories, proximities, "post_proc_output.avi")
+        # print('Annotated video saved to {}'.format("post_proc_output.avi"))
 
 
 if __name__ == '__main__' :
