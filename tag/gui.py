354994d0fef0a3d5e9775fcb199e282f5489fde4
Jesse Galloway <jgalloway@anduril.com>
2025-01-04 14:16:41 -0500

    format

--- tag/gui.py
+++ tag/gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import cv2
 from PIL import ImageTk, Image
+
 top = tk.Tk()
 
 # Code to add widgets will go here...
@@ -8,6 +9,7 @@ top = tk.Tk()
 # image = cv2.imread(path)
 
 # Open up a video from the sample video folder with OpenCV
+
 
 class VideoPlayer:
     def __init__(self, canvas, vid, original_image):
@@ -19,18 +21,19 @@ class VideoPlayer:
         print("Jump")
         self.vid.set(cv2.CAP_PROP_POS_FRAMES, 2000)
 
-
     def nextFrame(self):
         # I have no idea why we need original image
         print("Frame: {}".format(self.original_image))
         ret, frame = self.vid.read()
-        image = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
-        self.canvas.itemconfig(1, image = image)
+        image = ImageTk.PhotoImage(
+            Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        )
+        self.canvas.itemconfig(1, image=image)
         self.original_image = image
         self.canvas.after(50, self.nextFrame)
 
 
-canvas = tk.Canvas(top, width = 300, height = 300)
+canvas = tk.Canvas(top, width=300, height=300)
 
 
 vid = cv2.VideoCapture("../sample_videos/video1.mp4")
@@ -44,10 +47,9 @@ print("first image + {}".format(first_image))
 vp.nextFrame()
 
 
-
 greeting = tk.Label(text="Beetle Tracker GUI")
 button = tk.Button(text="Click me!", command=vp.jumpToFrame)
-  
+
 greeting.pack()
 button.pack()
 canvas.pack()
